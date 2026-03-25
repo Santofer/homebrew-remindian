63fd21560026e1c126acf791a1ff07c0f235c785
Amine Benboubker <amine@MacBookPro.localdomain>
2026-03-25 10:54:00 +0100

    Add Remindian v5.0.0 cask formula

---
+++ Casks/remindian.rb
@@ -0,0 +1,20 @@
+cask "remindian" do
+  version "5.0.0"
+  sha256 "2cfe58a44d9f39749fb43857f6e4ec82d17513091fe3208f7fa4736971b2aa67"
+
+  url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-v#{version}.dmg"
+  name "Remindian"
+  desc "Sync Obsidian tasks with Apple Reminders, Todoist, Asana, Linear, and more"
+  homepage "https://github.com/Santofer/Remindian"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Remindian.app"
+
+  zap trash: [
+    "~/Library/Application Support/Remindian",
+    "~/Library/Containers/com.remindian.app",
+    "~/Library/Preferences/com.remindian.app.plist",
+  ]
+end
